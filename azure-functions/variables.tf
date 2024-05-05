486d6f50aff9f3d26a378f4b94968972eaf338dd
Kamil Gatkowski <152792497+ekafrozen@users.noreply.github.com>
2024-05-05 17:47:39 +0200

    Update variables.tf

--- azure-functions/variables.tf
+++ azure-functions/variables.tf
@@ -1,1 +1,54 @@
+variable "name" {
+  description = "Nazwa"
+}
 
+variable "location" {
+  description = "Lokalizacja"
+}
+
+variable "account_tier" {
+  description = "Poziom konta"
+  default = ""
+}
+
+variable "account_replication_type" {
+  description = "Typ replikcaji konta"
+  default = ""
+}
+
+variable "environment" {
+  description = "Lokalizacja"
+}
+
+variable "os_type" {
+  description = "Typ opr"
+  default = ""
+}
+
+variable "sku_name" {
+  description = "Sku name"
+  default = ""
+}
+
+variable "function_app_id" {
+  description = "ID app"
+  default = ""
+}
+
+variable "language" {
+  description = "Jezyk"
+}
+
+variable "authLevel" {
+  description = "Poziom ident"
+  default = ""
+}
+
+variable "direction" {
+  description = "Lokalizacja"
+  default = ""
+}
+
+variable "type" {
+  description = "Typ"
+}
