ad328be18df491d990da314fb2607d7df4c2d44c
Kamil Gatkowski <152792497+ekafrozen@users.noreply.github.com>
2024-05-05 18:03:24 +0200

    Update variables.tf

--- azuer-blob-storage/variables.tf
+++ azuer-blob-storage/variables.tf
@@ -1,1 +1,42 @@
+variable "name" {
+  description = "Nazwa"
+}
+
+variable "location" {
+  description = "Lokalizacja"
+}
+
+variable "account_tier" {
+  description = "Poziom konta"
+  default = ""
+}
+
+variable "account_replication_type" {
+  description = "Typ replikcaji konta"
+  default = ""
+}
+
+variable "environment" {
+  description = "Lokalizacja"
+}
+
+variable "container_access_type" {
+  description = "Typ kontenera"
+  default = ""
+}
+
+variable "type" {
+  description = "Typ"
+  default = ""
+}
+
+variable "source" {
+  description = "Zrodlo"
+  default = ""
+}
+
+variable "authLevel" {
+  description = "Poziom ident"
+  default = ""
+}
 
